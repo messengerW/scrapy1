773da1c1f100f6ac4a646724990cad0cd99a4716
messengerW <messenger232624@gmail.com>
2019-09-25 22:54:03 +0800

    学习多线程爬虫

--- scrapy1/spiders/Premier/aaa_test.py
+++ scrapy1/spiders/Premier/aaa_test.py
@@ -1,19 +1,45 @@
-import re
+import time
+import threading
+import requests
 
-url = 'http://www.tzuqiu.cc/playerStatistics/querysStat.json?draw=2&columns%5B0%5D%5Bdata%5D=id&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=playerFormat&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=appsFormat&columns%5B2%5D%5Bname%5D=psp.appsCP&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=minsFormat&columns%5B3%5D%5Bname%5D=psp.minsCP&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=goalsFormat&columns%5B4%5D%5Bname%5D=psp.goalsCP&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=assistsFormat&columns%5B5%5D%5Bname%5D=psp.assistsCP&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=cardsFormat&columns%5B6%5D%5Bname%5D=psp.cardsCP&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=passSuccFormat&columns%5B7%5D%5Bname%5D=psp.passSuccCP&columns%5B7%5D%5Bsearchable%5D=true&columns%5B7%5D%5Borderable%5D=true&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B8%5D%5Bdata%5D=bigChanceCreatedFormat&columns%5B8%5D%5Bname%5D=psp.bigChanceCreatedCP&columns%5B8%5D%5Bsearchable%5D=true&columns%5B8%5D%5Borderable%5D=true&columns%5B8%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B8%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B9%5D%5Bdata%5D=aerialWonFormat&columns%5B9%5D%5Bname%5D=psp.aerialWonCP&columns%5B9%5D%5Bsearchable%5D=true&columns%5B9%5D%5Borderable%5D=true&columns%5B9%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B9%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B10%5D%5Bdata%5D=mansFormat&columns%5B10%5D%5Bname%5D=psp.mansCP&columns%5B10%5D%5Bsearchable%5D=true&columns%5B10%5D%5Borderable%5D=true&columns%5B10%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B10%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B11%5D%5Bdata%5D=rateFormat&columns%5B11%5D%5Bname%5D=psp.rateCP&columns%5B11%5D%5Bsearchable%5D=true&columns%5B11%5D%5Borderable%5D=true&columns%5B11%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B11%5D%5Bsearch%5D%5Bregex%5D=false&start=10&length=10&search%5Bvalue%5D=&search%5Bregex%5D=false&extra_param%5BcompetitionId%5D=1&extra_param%5Bseason%5D=19%2F20&extra_param%5BorderCdnReq%5D=true&_=1568866686557'
+link_list = []
 
-_draw = re.search(r"draw=(\d+)",url).group(1)
-_start = re.search(r"start=(\d+)",url).group(1)
-_value = re.search(r"&_=(\d+)",url).group(1)
+with open('uuurls.txt','r') as file:
+    file_list = file.readlines()
+    for each in file_list:
+        link = each.split('\t')[1]
+        link = link.replace('\n','')
+        link_list.append(link)
 
-_draw = 'draw=' + str(int(_draw)+1)
-_start = 'start=' + str(int(_start)+10)
-_value = '_=' + str(int(_value)+1)
-nexturl = re.sub(r"draw=(\d+)",_draw,url)
-nexturl = re.sub(r"start=(\d+)",_start,nexturl)
-nexturl = re.sub(r"_=(\d+)",_value,nexturl)
+start = time.time()
+class myThread(threading.Thread):
+    def __init__(self, name, link_range):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.link_range = link_range
+    def run(self):
+        print("Starting " + self.name)
+        crawler(self.name, self.link_range)
+        print("Ending " + self.name)
 
-# start = 'start=' + str(int(start)+20)
-# nexturl = re.sub(r"start=(\d+)",start,url)
+def crawler(threadName, link_range):
+    for i in range(link_range[0],link_range[1]+1):
+        try:
+            r = requests.get(link_list[i], timeout = 20)
+            print(threadName, r.status_code, link_list[i])
+        except Exception as e:
+            print(threadName,'Error:',e)
 
-print(nexturl)
+thread_list = []
+link_range_list = [(0,200),(201,400),(401,600),(601,800),(801,1000)]
+
+for i in range(1,6):
+    thread = myThread("Thread-"+str(i), link_range_list[i-1])
+    thread.start()
+    thread_list.append(thread)
+
+for thread in thread_list:
+    thread.join()
+
+end = time.time()
+print('Tatal time : ',end-start)
